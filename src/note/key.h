4ea8d0bc234abc9fd89a9c6539dafbb129e35b3e
Alexander Shultz <ashultzy@rams.colostate.edu>
2021-01-31 11:02:16 -0700

    Added getKey function

--- src/note/key.h
+++ src/note/key.h
@@ -4,6 +4,16 @@
 #include <Arduino.h>
 
 namespace note {
+/*! @brief Type of key. */
+enum class KeyType : byte { MAJOR, MINOR };
+/**
+ * Get the key for a root note.
+ * 
+ * @param type key type.
+ * @param root root note.
+ * @param key pointer to target destination.
+ */
+void getKey(KeyType type, byte root, byte* key);
 /**
  * Assign the major key for the provided root note to the provided
  * array pointer.
