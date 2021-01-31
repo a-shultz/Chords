45d71d8d58abb97f0056fd50b28ed8f9c361711a
Alex Shultz <31495397+a-shultz@users.noreply.github.com>
2021-01-31 10:52:43 -0700

    Split note file (#4)
    * Moved note functions
    
    * Moved key functions
    
    * Moved chord functions
    
    * Moved progression functions
    
    * Removed note.h and note.cpp

---
+++ src/note/chord.cpp
@@ -0,0 +1,35 @@
+#include "chord.h"
+
+namespace note {
+
+void getChord(ChordType type, byte rootIndex, byte* key, byte* chord) {
+    switch (type) {
+        case ChordType::TRIAD: 
+            getChordTriad(rootIndex, key, chord);
+            break;
+        case ChordType::SEVENTH:
+            getChordSeventh(rootIndex, key, chord);
+            break;
+    }
+}
+
+void getChordTriad(byte rootIndex, byte* key, byte* chord) {
+    chord[0] = key[rootIndex]; 
+    chord[1] = key[getNoteIndex(rootIndex + 2)];
+    chord[2] = key[getNoteIndex(rootIndex + 4)];
+}
+
+void getChordSeventh(byte rootIndex, byte* key, byte* chord) {
+    chord[0] = key[rootIndex]; 
+    chord[1] = key[getNoteIndex(rootIndex + 2)];
+    chord[2] = key[getNoteIndex(rootIndex + 4)];
+    chord[3] = key[getNoteIndex(rootIndex + 6)];
+}
+
+byte getNoteIndex(byte index) {
+    if (index < 8)
+        return index;
+    return index - 8;
+}
+
+} // namespace note
